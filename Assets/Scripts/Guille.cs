62161f7cc3c78d7d4b2ddb744083cdded7a617de
santiago-bo <santiballen02@gmail.com>
2020-10-16 12:47:37 -0500

    Movimiento de guille
    Movimientos basicos y que gire

--- Assets/Scripts/Guille.cs
+++ Assets/Scripts/Guille.cs
@@ -8,12 +8,22 @@ public class Guille : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        if (Input.GetKey("right"))
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.right, Vector3.up);
+            transform.position += Vector3.right * velocidad * Time.deltaTime;
+           
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.left, Vector3.up);
+            transform.position += Vector3.left * velocidad * Time.deltaTime;
+        }
     }
 }
