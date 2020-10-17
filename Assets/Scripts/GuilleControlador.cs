3ffc53733a5fcc779cfa498e9b78c64988fb65f6
Laudarias <72948114+Laudarias@users.noreply.github.com>
2020-10-17 16:33:21 -0500

    Salto guille
    Configuré el salto de guille

--- Assets/Scripts/GuilleControlador.cs
+++ Assets/Scripts/GuilleControlador.cs
@@ -6,27 +6,51 @@ public class GuilleControlador : MonoBehaviour
 {
     public float velocidad;
     public float rotacion;
+    public float salto;
+    public bool sobrePiso = true;
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
         if (Input.GetKey("right"))
         {
             transform.rotation = Quaternion.LookRotation(Vector3.right, Vector3.up);
             transform.position += Vector3.right * velocidad * Time.deltaTime;
-            transform.rotation = Quaternion.Euler(0.0f,Mathf.PingPong(Time.time * 50, rotacion*2)-280, 0.0f);
+
+            if (sobrePiso)
+            {
+                transform.rotation = Quaternion.Euler(0.0f,Mathf.PingPong(Time.time * 50, rotacion*2)-280, 0.0f);
+            }
         }
+
         if (Input.GetKey("left"))
         {
             transform.rotation = Quaternion.LookRotation(Vector3.left, Vector3.up);
             transform.position += Vector3.left * velocidad * Time.deltaTime;
-            transform.rotation = Quaternion.Euler(0.0f,Mathf.PingPong(Time.time * 50, rotacion*2)-100, 0.0f);
+            
+            if (sobrePiso)
+            {
+                transform.rotation = Quaternion.Euler(0.0f,Mathf.PingPong(Time.time * 50, rotacion*2)-100, 0.0f);
+            }
+        }
+
+        if (Input.GetButtonDown("Jump") && sobrePiso) {
+            rb.AddForce(Vector3.up*salto, ForceMode.Impulse);
+            sobrePiso = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision otro) {
+        if (otro.gameObject.tag == "Piso") {
+            sobrePiso = true;
         }
     }
 }
