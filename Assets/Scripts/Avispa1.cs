7cf2fc70d49f00030b93054e4602cf3241dbad06
santiago-bo <santiballen02@gmail.com>
2020-10-26 23:08:32 -0500

    Metí una avispa
    lol

---
+++ Assets/Scripts/Avispa1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avispa1 : MonoBehaviour
+{
+    public float velocidad;
+    // Start is called before the first frame update
+    Vector3 pos;
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos += transform.forward * Time.deltaTime * velocidad;
+        transform.position = pos + transform.up * Mathf.Sin(Time.time);
+    }
+}
