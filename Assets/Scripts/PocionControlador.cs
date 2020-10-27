72e971e2a13de94bc7c3b687e6cbfd807d5a4a6d
Abel Arismendy <39172024+abelarismendy@users.noreply.github.com>
2020-10-27 11:06:10 -0500

    fix merge, añadir pocion
    @Laudarias añadió la pocíon

---
+++ Assets/Scripts/PocionControlador.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PocionControlador : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(0.0f,40,0.0f) * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time/2, 0.5f) +0.7f, transform.position.z);
+    }
+}
