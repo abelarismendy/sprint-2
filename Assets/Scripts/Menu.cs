90d1dc02f5306178c6641d6e7db9ef8bade0c700
Abel Arismendy <39172024+abelarismendy@users.noreply.github.com>
2020-10-27 19:14:29 -0500

    script menu
    funciona!

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject Iniciar;
+    public GameObject Salir;
+
     void Start()
     {
-        
+        Button btnIniciar = Iniciar.GetComponent<Button>();
+		btnIniciar.onClick.AddListener(Jugar);
+        Button btnSalir = Salir.GetComponent<Button>();
+		btnSalir.onClick.AddListener(Cerrar);
+    }
+    void Jugar()
+    {
+        SceneManager.LoadScene("Nivel 1");
     }
 
-    // Update is called once per frame
-    void Update()
+    void Cerrar()
     {
-        
+        Application.Quit();
     }
 }
